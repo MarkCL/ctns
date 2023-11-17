07e84b81aa401da47edc9b77ae98f7095d13ef9e
MarkWu <markwu@iqhive.com>
2023-11-17 14:42:41 +1300

    change function name and add support for convert type.

--- ctns.go
+++ ctns.go
@@ -2,9 +2,11 @@ package ctns
 
 import "reflect"
 
-// ConvertToNewStruct converts a struct to another struct with same tag
+// ConvertToNewType converts a struct to another struct with same tag
+// if tagName is not passed into this function, it will convert T1 to T2 with forced transformation
 // tagName accepts json or other tag name which will be used to match fields
-func ConvertToNewStruct[T1, T2 any](obj T1, tagName string) T2 {
+// it will only take first tagName for conversion
+func ConvertToNewType[T1, T2 any](obj T1, tagName ...string) T2 {
 	sType := reflect.TypeOf(obj)
 	sValue := reflect.ValueOf(obj)
 	if sType.Kind() == reflect.Ptr {
@@ -14,7 +16,12 @@ func ConvertToNewStruct[T1, T2 any](obj T1, tagName string) T2 {
 		sType = sType.Elem()
 		sValue = sValue.Elem()
 	}
-	d_exist_map, d_idx_map, d := getDestinationInfo[T2](tagName)
+	var d T2
+	var d_exist_map map[string]bool
+	var d_idx_map map[string]int
+	if tagName != nil {
+		d_exist_map, d_idx_map, d = getDestinationInfo[T2](tagName[0])
+	}
 	dType := reflect.TypeOf(d)
 	dKind := dType.Kind()
 	if dKind == reflect.Ptr {
@@ -23,12 +30,17 @@ func ConvertToNewStruct[T1, T2 any](obj T1, tagName string) T2 {
 	} else {
 		d = reflect.New(reflect.TypeOf([1]T2{})).Elem().Index(0).Interface().(T2)
 	}
+
+	if tagName == nil {
+		return sValue.Convert(dType).Interface().(T2)
+	}
+
 	var dValue reflect.Value
 	if dKind == reflect.Ptr {
 		dValue = reflect.ValueOf(d).Elem()
 	} else {
 		dValue = reflect.ValueOf(&d).Elem()
 	}
-	setDestinationStructValue(sType, sValue, dType, dValue, d_exist_map, d_idx_map, tagName)
+	setDestinationStructValue(sType, sValue, dType, dValue, d_exist_map, d_idx_map, tagName[0])
 	return d
 }
--- readme.md
+++ readme.md
@@ -1,6 +1,8 @@
 # ConvertToNewStruct 
     converts a struct to another struct with same tag
+    if tagName is not passed into this function, it will convert T1 to T2 with forced transformation
     tagName accepts json or other tag name which will be used to match fields
+    it will only take first tagName for conversion
     a simple main.go example for using this function:
 ```
 package main
@@ -23,9 +25,16 @@ type Test2 struct {
 	Ad string `json:"address"`
 }
 
+type Test3 string
+
+type Test4 string
+
 func main() {
-	t1 := Test{Name: "test", Age: 10, Address: "address", Mobile: "mobile"}
-	t2 := ctns.ConvertToNewStruct[Test, *Test2](t1, "json")
+	t1 := &Test{Name: "test", Age: 10, Address: "address", Mobile: "mobile"}
+	t2 := ctns.ConvertToNewType[*Test, *Test2](t1, "json")
+	var t3 Test3 = "ttt"
+	t4 := ctns.ConvertToNewType[Test3, Test4](t3)
 	fmt.Println(t2)
+	fmt.Println(t4)
 }
 ```
